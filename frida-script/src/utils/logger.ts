0a73a0cbfce2f399c875fa1658de59b5e91df903
repinek <137826826+repinek@users.noreply.github.com>
2025-12-20 19:53:43 +0500

    style: format logger.ts

--- frida-script/src/utils/logger.ts
+++ frida-script/src/utils/logger.ts
@@ -15,55 +15,55 @@ export class Logger {
         CYAN: "\x1b[36m",
         YELLOW: "\x1b[33m",
         RED: "\x1b[31m"
-    } as const;
+    } as const
 
     private static getTime(): string {
-        const date = new Date();
-        const hh = date.getHours().toString().padStart(2, "0");
-        const mm = date.getMinutes().toString().padStart(2, "0");
-        const ss = date.getSeconds().toString().padStart(2, "0");
-        const ms = date.getMilliseconds().toString().padStart(3, "0");
-        return `${this.Colors.GRAY}[${hh}:${mm}:${ss}.${ms}]${this.Colors.RESET}`;
+        const date = new Date()
+        const hh = date.getHours().toString().padStart(2, "0")
+        const mm = date.getMinutes().toString().padStart(2, "0")
+        const ss = date.getSeconds().toString().padStart(2, "0")
+        const ms = date.getMilliseconds().toString().padStart(3, "0")
+        return `${Logger.Colors.GRAY}[${hh}:${mm}:${ss}.${ms}]${Logger.Colors.RESET}`
     }
 
     private static log(type: LogType, ...messages: any[]): void {
-        const time = this.getTime();
+        const time = Logger.getTime()
         switch (type) {
             case LogType.INFO:
-                console.info(`${time} ${this.Colors.BLUE}[INFO]${this.Colors.RESET}`, ...messages);
+                console.info(`${time} ${Logger.Colors.BLUE}[INFO]${Logger.Colors.RESET}`, ...messages)
                 break
             case LogType.INFO_GREEN:
-                console.info(`${time} ${this.Colors.GREEN}[INFO]`, ...messages, this.Colors.RESET);
+                console.info(`${time} ${Logger.Colors.GREEN}[INFO]`, ...messages, Logger.Colors.RESET)
                 break
             case LogType.WARN:
-                console.warn(`${time} ${this.Colors.YELLOW}[WARN]${this.Colors.RESET}`, ...messages);
+                console.warn(`${time} ${Logger.Colors.YELLOW}[WARN]${Logger.Colors.RESET}`, ...messages)
                 break
             case LogType.ERROR:
-                console.error(`${time} ${this.Colors.RED}[ERROR]${this.Colors.RESET}`, ...messages);
+                console.error(`${time} ${Logger.Colors.RED}[ERROR]${Logger.Colors.RESET}`, ...messages)
                 break
             case LogType.HOOK:
-                console.debug(`${this.getTime()} ${this.Colors.GRAY}[HOOK]`, ...messages, this.Colors.RESET);
+                console.debug(`${Logger.getTime()} ${Logger.Colors.GRAY}[HOOK]`, ...messages, Logger.Colors.RESET)
                 break
-       }
+        }
     }
 
     static info(...messages: any[]) {
-        this.log(LogType.INFO, ...messages);
+        Logger.log(LogType.INFO, ...messages)
     }
 
     static infoGreen(...messages: any[]) {
-        this.log(LogType.INFO_GREEN, ...messages);
+        Logger.log(LogType.INFO_GREEN, ...messages)
     }
 
     static warn(...messages: any[]) {
-        this.log(LogType.WARN, ...messages);
+        Logger.log(LogType.WARN, ...messages)
     }
 
     static error(...messages: any[]) {
-        this.log(LogType.ERROR, ...messages);
+        Logger.log(LogType.ERROR, ...messages)
     }
 
     static hook(...messages: any[]) {
-        this.log(LogType.HOOK, ...messages);
+        Logger.log(LogType.HOOK, ...messages)
     }
 }
